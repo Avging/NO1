31214c15ac3ec9e7d398c8d8debdd0e44cd9dac8
Han <han@say521.cn>
2022-12-13 16:13:44 +0800

    第二天测试

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -147,6 +147,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/Study/cproject/study-1/cmake-build-debug/woo/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Study/cproject/study-1/cmake-build-debug/day21/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
---
+++ day21/main.c
@@ -0,0 +1,10 @@
+//
+// Created by Han on 2022/12/13.
+//
+#include <stdio.h>
+
+int main(void)   {
+    printf("123");
+
+    return 0;
+}
